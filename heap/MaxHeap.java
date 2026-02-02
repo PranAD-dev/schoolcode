f3a8bf4f7473b756edc75afb064b17912bc5f415
PranAD-dev <pranjal.adhikari@sjsu.edu>
2026-02-01 20:01:10 -0800

    insert heap implemented

---
+++ heap/MaxHeap.java
@@ -0,0 +1,115 @@
+package heap;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class MaxHeap
+{
+   private ArrayList<Student> students;
+   
+   public MaxHeap(int capacity)
+   {
+      students = new ArrayList<Student>(capacity);
+   }
+      
+   public MaxHeap(Collection<Student> collection)
+   {
+      students = new ArrayList<Student>(collection);
+      for(int i = size()/2 - 1; i >= 0; i--)
+      {
+         maxHeapify(i);
+      }
+   }
+   
+   public Student getMax()
+   {
+      if(size() < 1)
+      {
+         throw new IndexOutOfBoundsException("No maximum value:  the heap is empty.");
+      }
+      return students.get(0);
+   }
+   
+   public Student extractMax()
+   {
+      Student value = getMax();
+      students.set(0,students.get(size()-1));
+      students.remove(size()-1);
+      maxHeapify(0);
+      return value;
+   }
+    
+   public int size()
+   {
+      return students.size();
+   }
+   private void insertionHelper(int index){
+      int parent = parent(index);
+      if (students.get(parent).compareTo(students.get(index)) < 0) {
+        Student temp = students.get(parent); 
+        students.set(parent, students.get(index));
+        students.set(index, temp);
+        insertionHelper(parent);
+      }
+
+   }
+   public void insert(Student elt)
+   {
+      
+      //Please write me.  I should add the given student into the heap,
+	  //following the insert algorithm from the videos.
+      
+      students.add(elt);
+      insertionHelper(students.size()-1);
+      
+   }
+   
+   public void addGrade(Student elt, double gradePointsPerUnit, int units)
+   {
+      //Please write me.  I should change the student's gpa (using a method
+	  //from the student class), and then adjust the heap as needed using
+	  //the changeKey algorithm from the videos.
+   }
+   
+   private int parent(int index)
+   {
+      return (index - 1)/2;
+   }
+   
+   private int left(int index)
+   {
+      return 2 * index + 1;
+   }
+   
+   private int right(int index)
+   {
+      return 2 * index + 2;
+   }
+   
+   private void swap(int from, int to)
+   {
+      Student val = students.get(from);
+      students.set(from,  students.get(to));
+      students.set(to,  val);
+   }
+   
+   private void maxHeapify(int index)
+   {
+      int left = left(index);
+      int right = right(index);
+      int largest = index;
+      if (left <  size() && students.get(left).compareTo(students.get(largest)) > 0)
+      {
+         largest = left;
+      }
+      if (right <  size() && students.get(right).compareTo(students.get(largest)) > 0)
+      {
+         largest = right;
+      }
+      if (largest != index)
+      {
+         swap(index, largest);
+         maxHeapify(largest);
+      }  
+   }   
+}
